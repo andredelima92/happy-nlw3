1092ded2e9396d1149d61c831b99306a4d70def6
Andre <andredelima92@gmail.com>
2020-10-13 20:28:45 -0300

    bd, models and contnd controllers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,10 @@
 import express from "express";
+import "./database/connection";
+
+import routes from "./routes";
 
 const app = express();
 app.use(express.json());
-
-app.get("/users/:id", (req, res) => {
-  
-});
+app.use(routes);
 
 app.listen(3333);
