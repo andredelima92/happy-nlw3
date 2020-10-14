82d3bcb42ebb4fd89303cafa7dd4fe5fa73881ab
Andre <andredelima92@gmail.com>
2020-10-14 18:55:52 -0300

    components

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -2,19 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import Leaflet from 'leaflet';
-
-import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/orphanages-map.css';
-
-const mapIcon = Leaflet.icon({
-  iconUrl: mapMarkerImg,
-  iconSize: [58, 68],
-  iconAnchor: [29, 38],
-  popupAnchor: [170, 2],
-});
+import mapIcon from '../utils/mapIcon';
 
 const OrphanagesMap: React.FC = () => {
   return (
@@ -41,14 +32,14 @@ const OrphanagesMap: React.FC = () => {
         <Marker icon={mapIcon} position={[-22.21526, -49.971377]}>
           <Popup className="map-popup" closeButton={false} minWidth={240} maxWidth={240}>
             Lar das meninas
-            <Link to="">
+            <Link to="orphanages/1">
               <FiArrowRight size={20} color="#fff" />
             </Link>
           </Popup>
         </Marker>
       </Map>
 
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
       </Link>
     </div>
