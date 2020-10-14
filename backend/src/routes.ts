7b2bf0eab257e0940bbc729e141637969ffb5795
Andre <andredelima92@gmail.com>
2020-10-13 23:52:04 -0300

    insert orphanages,  images and validations

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
+
+import UploadConfig from "./config/upload";
 import OrphanagesController from "./controllers/OrphanagesController";
 
 const routes = Router();
+const upload = multer(UploadConfig);
 
 routes.get("/orphanages", OrphanagesController.index);
-routes.post("/orphanages", OrphanagesController.create);
+routes.get("/orphanages/:id", OrphanagesController.show);
+routes.post("/orphanages", upload.array('images'), OrphanagesController.create);
 
 export default routes;
---
+++ backend/src/views/images_view.ts
@@ -0,0 +1,25 @@
+import Image from "../models/Image";
+
+export default {
+  render(image: Image) {
+    return {
+      id: image.id,
+      url: `http://127.0.0.1:3333/uploads/${image.path}`,
+    };
+  },
+
+  renderMany(images: Image[]) {
+    return images.map((image) => this.render(image));
+  },
+};
+
+// images: [
+//   {
+//     id: 1,
+//     path: "1602641299198-previa2.png",
+//   },
+//   {
+//     id: 2,
+//     path: "1602641299199-previa.jpeg",
+//   },
+// ],
